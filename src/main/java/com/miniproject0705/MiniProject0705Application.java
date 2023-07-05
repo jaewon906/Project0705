de13bc7606789fac6f3ab8083b7ad3aac310de0b
Park Jae Won <ploi9@naver.com>
2023-07-05 23:57:19 +0900

    23.07.05 start

---
+++ src/main/java/com/miniproject0705/MiniProject0705Application.java
@@ -0,0 +1,13 @@
+package com.miniproject0705;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniProject0705Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiniProject0705Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/miniproject0705/MiniProject0705ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miniproject0705;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniProject0705ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
