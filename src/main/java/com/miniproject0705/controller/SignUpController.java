8d00c01893f6be8c2a6914923119108acad6eab4
Park Jae Won <ploi9@naver.com>
2023-07-07 00:06:25 +0900

    hihi

--- src/main/java/com/miniproject0705/controller/SignUpController.java
+++ src/main/java/com/miniproject0705/controller/SignUpController.java
@@ -2,11 +2,9 @@ package com.miniproject0705.controller;
 
 import com.miniproject0705.domain.MemberInfoDTO;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class SignUpController {
